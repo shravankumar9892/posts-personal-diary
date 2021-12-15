dac6e696644819237eccc55776d4ce340e12acee
Shravan Shetty <shravankumarshetty9892@gmail.com>
2021-12-15 23:20:29 +0530

    posts service updated

--- index.js
+++ index.js
@@ -2,26 +2,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 posts = {};
-app.get("/", (req, res) => {
-  res.status(201).send("Hello World");
-});
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
+// app.get("/posts", (req, res) => {
+//   res.send(posts);
+// });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
   posts[id] = { id, title };
 
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "PostCreated",
+      data: {
+        id,
+        title,
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
